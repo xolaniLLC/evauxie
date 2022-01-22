b8b251c21bf9d48b652c1848b340b521848115f1
Momina-11821 <68692133+Momina-11821@users.noreply.github.com>
2022-01-23 01:40:49 +0500

    first code
    5 pages

---
+++ Assets/jquery/blogs-page.js
@@ -0,0 +1,15 @@
+
+//   navbutton toggle
+document.getElementById("nav-toggle-btn").addEventListener("click", tooglenav);
+function tooglenav(){
+	document.getElementById("side-nav").classList.toggle("center-links-toggle");
+}
+document.getElementById("nav-toggle-btn-2").addEventListener("click", tooglenav);
+function tooglenav(){
+	document.getElementById("side-nav").classList.toggle("center-links-toggle");
+}
+
+//   navbutton toggle
+
+
+
