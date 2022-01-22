d14864e1ba5d278e3a8eec5971fe1b138b814b17
Momina-11821 <68692133+Momina-11821@users.noreply.github.com>
2022-01-23 01:05:12 +0500

    Revert "Revert "update 22 January 2022""
    This reverts commit 8ec4b786e40887a326a118885d1945998b51b9ea.

---
+++ Assets/jquery/scroolspy.js
@@ -0,0 +1,13 @@
+(function($){
+	"use strict"
+	$('#scrool').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+	
+});
+	
+});(jQuery);
